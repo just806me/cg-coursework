2874e5615ded28cf668e8873cf70a7da02a2a7d8
Just Me <just806me@gmail.com>
2019-03-16 22:30:30 +0200

    removed scrollreveal

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 $(function () {
-  ScrollReveal({ duration: 800, reset: true, mobile: false })
-
   $('.header .item').on('click', function () {
     var $this = $(this), page = $this.data('page')
 
